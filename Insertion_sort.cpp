67d841362ae8eef62b7c0385437d099535253be1
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-11-01 22:24:25 +0600

    01-11-2024

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -23,9 +23,22 @@ void solve(void)
 {
     ll n;
     cin >> n;
-    vector<ll> v(n);
-    for (auto &it : v)
-        cin >> it;
+    ll a[n + 1];
+    for (ll i = 1; i <= n; i++)
+        cin >> a[i];
+    for (ll i = 1; i <= n; i++)
+    {
+        ll temp = a[i];
+        ll j = i - 1;
+        while (j >= 1 && temp < a[j])
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = temp;
+    }
+    for (ll i = 1; i <= n; i++)
+        cout << a[i] << ' ';
 }
 //------------------------------------------------------------------------------------------
 int main()
