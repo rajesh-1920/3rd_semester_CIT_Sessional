9eb29e91ebadee8d1b2b7cb1822672b81bf7d6b9
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-11-04 20:41:08 +0600

    04-11-2024

--- (sort)Topological_sort.cpp
+++ (sort)Topological_sort.cpp
@@ -15,17 +15,51 @@ typedef long long int ll;
 #define sc second
 #define pb(x) push_back(x)
 #define ppb(x, y) push_back({x, y})
+#define all(x) x.begin(), x.end()
 
 #define inf 90000000000000
 #define N 200009
 //------------------------------------------------------------------------------------------
+vector<ll> g[N], topo;
+ll vis[N], cycle;
+void dfs(ll n)
+{
+    vis[n] = 1;
+    for (auto it : g[n])
+    {
+        if (vis[it] == 1)
+            cycle = 1;
+        if (vis[it] == 0)
+            dfs(it);
+    }
+    vis[n] = 2;
+    topo.push_back(n);
+}
 void solve(void)
 {
-    ll n;
-    cin >> n;
-    vector<ll> v(n);
-    for (auto &it : v)
-        cin >> it;
+    ll n, m;
+    cin >> n >> m;
+    while (m--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        g[x].push_back(y);
+    }
+    cycle = 0;
+    for (ll i = 1; i <= n; i++)
+    {
+        if (vis[i] == 0)
+            dfs(i);
+    }
+    reverse(all(topo));
+    if (cycle == 1)
+        cout << "Cycle exist .";
+    else
+    {
+        cout << "The topological sortest list : ";
+        for (auto it : topo)
+            cout << it << ' ';
+    }
 }
 //------------------------------------------------------------------------------------------
 int main()
