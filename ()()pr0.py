bce76363b35019bdc77928aa43b749b1b56cef25
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-09-21 02:56:20 +0600

    5th

--- ()()pr0.py
+++ ()()pr0.py
@@ -72,3 +72,46 @@ relation.add("b", "Real CR", "lazy")
 
 relation.build_statement("A(s) s")
 relation.build_statement("a & !b ~ a")
+
+
+class People:
+    def __init__(self, name, parent=None, children=None):
+        self.name = name
+        self.parent = parent
+        self.children = [children]
+
+class Relation:
+    def __init__(self):
+        self.map = {}
+
+    def add(self, person):
+        self.map[person.name] = person
+
+    def parent(self, parent, child):
+        self.map[child].parent = parent
+        if self.map[parent].children == [None]:
+            self.map[parent].children = [child]
+        else:
+            self.map[parent].children.append(child)
+    
+    def find_grandparent(self, name):
+        parent = self.map[name].parent
+        if parent is None:
+            return None
+        grandparent = self.map[parent].parent
+        return grandparent
+    
+relation = Relation()
+
+john = People("John")
+mary = People("Mary")
+joe = People("Joe")
+
+relation.add(john)
+relation.add(mary)
+relation.add(joe)
+
+relation.parent("John", "Mary")
+relation.parent("Mary", "Joe")
+
+print("Joe's grandparent is:", relation.find_grandparent("Joe"))
